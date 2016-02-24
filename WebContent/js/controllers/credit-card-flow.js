06e404597f9c40aa0a93b3021a4ad50732bdfcff
Denis Ricci <denis.riccis@gmail.com>
2016-02-23 22:11:57 -0300

    Credit Card Flow View

---
+++ WebContent/js/controllers/credit-card-flow.js
@@ -0,0 +1,84 @@
+angular.module('money').controller('CashFlowController', function ($http, $routeParams, $scope) {
+    $scope.categories = [];
+    $scope.movements = [];
+    $scope.movement = {};
+    $scope.creditCard = {};
+    $scope.values = {};
+    $scope.values.positive = 0;
+    $scope.values.negative = 0;
+    $scope.map = {};
+    $scope.filter = {};
+
+    $scope.loaDefaultValues = function () {
+        $scope.movement.date = currentDate();
+        $scope.filter.start = firstDateOfMonth();
+        $scope.filter.end = lastDateOfMonth();
+    }
+
+    $scope.getCreditCard= function () {
+        $http.get(
+            'creditCard/creditCardById?creditCardId='
+            + $scope.creditCard.id).success(
+            function (data) {
+                $scope.bankAccount = data.bankAccount;
+            });
+    }
+
+    $scope.movementsList = function () {
+        $scope.values = {};
+        $scope.values.positive = 0;
+        $scope.values.negative = 0;
+        $scope.map = {};
+        var url = 'creditCard/movements?creditCardId=' + $scope.creditCard.id +
+            '&startDt=' + $scope.filter.start +
+            '&endDt=' + $scope.filter.end;
+        $http.get(url).success(
+            function (data) {
+                $scope.movements = data.list;
+                $scope.values.balance = $scope.movements[$scope.movements.length-1].currentBalance;
+            });
+    }
+
+    $scope.add = function () {
+        $scope.movement.idBankAccount = $scope.bankAccount.id;
+        $scope.movement.value = parseFloat($scope.movement.value);
+
+        $http.post('cashFlow/add', $scope.movement).success(
+            function (data) {
+                $scope.movementsList();
+                $scope.movement = {};
+                $scope.loaDefaultValues();
+                $scope.getAccount();
+            });
+    }
+
+    $scope.consolidate = function (movement) {
+        if (movement.value > 0) {
+            $scope.values.positive = $scope.values.positive
+                + movement.value;
+        } else {
+            $scope.values.negative = $scope.values.negative
+                + movement.value;
+        }
+
+        if (movement.category.description in $scope.map === true) {
+            $scope.map[movement.category.description] = $scope.map[movement.category.description]
+                + movement.value;
+        } else {
+            $scope.map[movement.category.description] = movement.value;
+        }
+    }
+
+
+    $scope.initCashFlow = function () {
+        $scope.bankAccount.id = $routeParams.accountId;
+
+        $scope.loaDefaultValues();
+        $scope.getAccount();
+        $scope.movementsList();
+
+        $http.get('category/list').success(function (data) {
+            $scope.categories = data.list;
+        });
+    }
+});
